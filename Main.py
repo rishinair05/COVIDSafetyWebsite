2803068539b07584a209ee965650a132a43e7493
RenshiNX <72934053+RenshiNX@users.noreply.github.com>
2022-02-04 20:20:25 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,11 @@ age = int(input("Please enter your age?"))
 factor = int(input("How many diseases/conditions do u have?"))
 factor = factor*10
 
-vulnerability = age + factor
+vul = age + factor
 
 print("Your vulnerability score is " + str(vulnerability))
+
+if vul > 200:
+    print("sahhh, uda stay indoors")
+else:
+    print("you gud m8 go enjoy life!")
